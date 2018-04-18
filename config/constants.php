37bf77ede3ff1039e8ab8a3c86ff4c2579cd1fe4
benny <303810616@qq.com>
2018-04-18 07:30:04 +0000

    更改PRIVATE_KEY

--- config/constants.php
+++ config/constants.php
@@ -10,7 +10,7 @@ return [
     /**
      * 易接相关参数
      */
-    'PRIVATE_KEY' => '',
+    'PRIVATE_KEY' => 'J21OKMXDBJ4P7KEFYW4F2IGSHRTCUN2K',
     'LOGIN_RESULT_SUCCESS' => 0,
     'CHECK_LOGIN_URL' => 'http://sync.1sdk.cn/login/check.html?',
 ];
