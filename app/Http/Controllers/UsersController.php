ed24d92205849f5c85a2d275fe55f8fdc2b29958
benny <303810616@qq.com>
2018-05-07 17:38:24 +0800

    新增users控制器/修改users表结构

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
+
+class UsersController extends Controller
+{
+    public function __construct(){
+
+    }
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,10 @@ Route::post('/1sdk/login', 'EsdkController@login');
 Route::get('/1sdk/notify', 'EsdkController@notify');
 Route::post('/1sdk/client/notify', 'EsdkController@clientNotify');
 
+/**
+ * 用户信息相关接口
+ */
+Route::post('/users/coin', 'UsersController@coin');
+
 //  临时接口，测试用
 Route::get('/1sdk/tools', 'EsdkController@testTools');
